7a52680a895801b500ceda3d99da2a205c1ce4c2
= <killikomfr@gmail.com>
2022-11-18 08:45:40 +0100

    Testing recursive display
    Testng a function to display object recursively in HTML.
    Still in developement, object display themselves in infinite loops and strings are considered objects.

--- script/app.js
+++ script/app.js
@@ -1,30 +1,41 @@
 const windowDiv = document.querySelector('#window');
 const navigatorDiv = document.querySelector('#navigator');
 
-function displayInfos (destination, objectToDisplay) {
+function displayInfos (destination, objectToDisplay, name = "", level = 0) {
 
-	console.log(objectToDisplay, typeof objectToDisplay);
+	if (level <= 4)
+	{
+		console.log(objectToDisplay, typeof objectToDisplay);
+		console.group();
 
-	let list = objectToDisplay;
+		let list = objectToDisplay;
 
-	if (typeof objectToDisplay === 'object') {
-		list = Object.keys(objectToDisplay);
-	}
-
-	list.forEach(element => {
-
-		let divElem = document.createElement('div');
-		divElem.className = "element";
-
-		console.log(typeof objectToDisplay[element])
-
-		if (typeof objectToDisplay[element] === 'object') {
-			displayInfos(divElem, element);
-		} else {
-			divElem.textContent = objectToDisplay[element];
+		if (typeof objectToDisplay === 'object') {
+			list = Object.keys(objectToDisplay);
 		}
-		destination.append(divElem);
-	});
+
+		list.forEach(element => {
+
+			let divElem = document.createElement('div');
+			divElem.className = "element";
+
+			console.log(typeof objectToDisplay[element])
+
+			if (typeof objectToDisplay[element] === 'object' && element !== name && element !== 'self') {
+				console.group();
+				console.log(element, objectToDisplay[element]);
+				displayInfos(divElem, objectToDisplay[element], element, level + 1);
+				console.groupEnd();
+			}
+
+			 /*else {*/
+				divElem.textContent = objectToDisplay[element];
+			//}
+			destination.append(divElem);
+		});
+
+		console.groupEnd();
+	}
 }
 
 function displayDimensions () {
@@ -34,7 +45,9 @@ function displayDimensions () {
 	`;
 }
 
-/*
-displayInfos(windowDiv, window);*/
 
-setInterval(displayDimensions, 10);
+/*displayInfos(windowDiv, window);*/
+
+/*setInterval(displayDimensions, 10);*/
+
+console.log(Object.keys(window));
